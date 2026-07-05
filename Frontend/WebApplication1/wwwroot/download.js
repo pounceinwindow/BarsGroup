3e7574308ce8e2a8a568eea9521422124b9dd483
pounceinwindow <adk03042006@gmail.com>
2026-07-05 21:40:18 +1000

    feat: download interview protocol PDF

---
+++ Frontend/WebApplication1/wwwroot/download.js
@@ -0,0 +1,13 @@
+window.downloadFileFromStream = async (fileName, contentStreamReference) => {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer], { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement("a");
+
+    anchor.href = url;
+    anchor.download = fileName;
+    document.body.appendChild(anchor);
+    anchor.click();
+    anchor.remove();
+    URL.revokeObjectURL(url);
+};
